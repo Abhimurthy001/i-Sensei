1b1891a5ebb9bc9cad20ece5297a805af4624998
Abhimurthy001 <abhishekmurthy2301@gmail.com>
2024-05-02 21:23:25 +0530

    fix - 032
    final fix, ready for demo

---
+++ aws/synthesize_speech.py
@@ -0,0 +1,41 @@
+import boto3
+import streamlit as st
+from contextlib import closing
+import os
+import sys
+import subprocess
+from tempfile import gettempdir
+from dotenv import load_dotenv
+
+load_dotenv()
+
+aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
+aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+
+
+Session = boto3.Session(
+        aws_access_key_id = aws_access_key_id,
+        aws_secret_access_key = aws_secret_access_key,
+        region_name = "us-east-1"
+    )
+
+def synthesize_speech(text):
+    Polly = Session.client("polly")
+    response = Polly.synthesize_speech(
+        Text=text,
+        OutputFormat="mp3",
+        VoiceId="Joanna")
+    if "AudioStream" in response:
+        with closing(response["AudioStream"]) as stream:
+            output = os.path.join(gettempdir(), "temp_audio.mp3")
+
+            try:
+                with open(output, "wb") as file:
+                    file.write(stream.read())
+            except IOError as error:
+                print(error)
+                sys.exit(-1)
+    else:
+        print("Could not stream audio")
+        sys.exit(-1)
+    return output
---
+++ speech_recognition/openai_whisper.py
@@ -0,0 +1,26 @@
+import openai
+import os
+openai.api_key = os.getenv("OPENAI_API_KEY")
+import wave
+from dotenv import load_dotenv
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+class Config:
+    channels = 2
+    sample_width = 2
+    sample_rate = 44100
+
+def save_wav_file(file_path, wav_bytes):
+    with wave.open(file_path, 'wb') as wav_file:
+        wav_file.setnchannels(Config.channels)
+        wav_file.setsampwidth(Config.sample_width)
+        wav_file.setframerate(Config.sample_rate)
+        wav_file.writeframes(wav_bytes)
+
+def transcribe(file_path):
+    audio_file = open(file_path, 'rb')
+    transcription = openai.Audio.transcribe("whisper-1", audio_file, api_key=OPENAI_API_KEY)
+    return transcription['text']
+
